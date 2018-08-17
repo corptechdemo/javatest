9de9ebb5b29886f4c5a2c8574ff89e402bf069c1
Masood Khan <30580844+corptechweb@users.noreply.github.com>
2018-08-17 14:17:46 -0600

    Initial Commit 0.2 - Added printing of Hello World

--- src/net/corptechsolutions/demo/Main.java
+++ src/net/corptechsolutions/demo/Main.java
@@ -3,6 +3,8 @@ package net.corptechsolutions.demo;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// Code here
+        System.out.println("Hello World");
+
     }
 }
